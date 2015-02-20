e549f6c86afd07a78850cf3a4f2b47bd23957000
Suguru Odai <ourqwers@gmail.com>
2015-02-20 16:13:51 +0900

    Add local database class

---
+++ lib/eng_cooker/database/local_file.rb
@@ -0,0 +1,37 @@
+module EngCooker
+  module Database
+    require 'json'
+
+    class LocalFile
+      def initialize(storage_path: 'db/sentences.json')
+        @storage_path = [APP_ROOT, storage_path].join('/')
+        @sentences = load_storage_file
+      end
+
+      def set(data)
+        @sentences.push(data.to_h)
+        File.open(@storage_path, 'w') { |file| file.write(@sentences.to_json) }
+      end
+
+      def sample
+        @sentences.sample
+      end
+
+      private
+
+      def load_storage_file
+        File.exist?(@storage_path) ? JSON.parse(File.read(@storage_path)) : []
+      rescue JSON::ParserError
+        raise StorageLoadError, 'データベースファイルの解析に失敗しました。'
+      end
+    end
+
+    class StorageLoadError < StandardError; end
+  end
+
+  Sentence.class_eval do
+    def to_h
+      { 'en_text' => @en_text, 'ja_text' => @ja_text, 'created_at' => Time.now }
+    end
+  end
+end
---
+++ spec/lib/eng_cooker/database/local_file_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+require 'eng_cooker/database/local_file'
+
+module EngCooker
+  describe Sentence do
+    let(:sentence) { Sentence.new('I had expected you to answer.', 'あなたに答えて欲しかった') }
+    subject { sentence }
+
+    before do
+      allow(Time).to receive(:now).and_return(Time.parse('2015/1/1 12:00:00'))
+    end
+
+    describe '#to_h' do
+      it 'should return the sentence hash' do
+        hash = sentence.to_h
+        expect(hash['en_text']).to eq 'I had expected you to answer.'
+        expect(hash['ja_text']).to eq 'あなたに答えて欲しかった'
+        expect(hash['created_at']).to eq Time.parse('2015/1/1 12:00:00')
+      end
+    end
+  end
+
+  describe Database::LocalFile do
+    it_should_behave_like 'a database strategy', EngCooker::Database::LocalFile
+  end
+end
