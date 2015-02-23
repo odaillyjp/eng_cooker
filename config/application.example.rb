1c7bfe58d02862501e4381fd4c0ebcc827974739
Suguru Odai <ourqwers@gmail.com>
2015-02-23 10:45:49 +0900

    Add binary file

---
+++ config/application.example.rb
@@ -0,0 +1,8 @@
+EngCooker.configure do |config|
+  config.database_adapter = 'local_file'
+  config.database_options = { storage_path: 'db/words.json' }
+
+  # Firebaseの場合
+  # config.database_adapter = 'firebase'
+  # config.database_options = { app_name: 'example' }
+end
